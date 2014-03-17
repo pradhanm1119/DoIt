c148a2e7a0cced437fdb92a45d9e7981829b6698
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-17 13:12:37 -0500

    Initial Commit

---
+++ DoIt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DoIt
+//
+//  Created by Manas Pradhan on 3/17/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
